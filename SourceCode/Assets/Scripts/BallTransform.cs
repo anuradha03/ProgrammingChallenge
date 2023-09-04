e908ac69ce4231672c78aa663b596931e1e2a893
anuradha03 <30650022+anuradha03@users.noreply.github.com>
2023-09-03 21:06:40 -0700

    Source code commit

---
+++ SourceCode/Assets/Scripts/BallTransform.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallTransform : MonoBehaviour
+{
+    // Start is called before the first frame update
+    // public Vector3 scaleChange;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localScale += new Vector3(0.001f,0.001f,0.001f);
+
+    }
+}
